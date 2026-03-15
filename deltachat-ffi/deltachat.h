53bd2bc19c618aca93e0dab9843e6bb7599e2c0f
rvv <5573352@gmail.com>
2026-03-15 23:37:06 +0200

    feat: add constant for group description change event

--- deltachat-ffi/deltachat.h
+++ deltachat-ffi/deltachat.h
@@ -4667,6 +4667,7 @@ uint32_t        dc_msg_get_info_contact_id    (const dc_msg_t* msg);
 #define         DC_INFO_INVALID_UNENCRYPTED_MAIL  13
 #define         DC_INFO_WEBXDC_INFO_MESSAGE       32
 #define         DC_INFO_CHAT_E2EE                 50
+#define         DC_INFO_GROUP_DESCRIPTION_CHANGED 70
 
 
 /**
